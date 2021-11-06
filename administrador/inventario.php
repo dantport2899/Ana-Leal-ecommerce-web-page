4b555f00dfd1375508f417c6b5f8f38c7da19b5b
Dante Portales <56794666+dantport2899@users.noreply.github.com>
2021-11-06 16:07:35 -0600

    Add files via upload

--- administrador/inventario.php
+++ administrador/inventario.php
@@ -53,6 +53,10 @@ session_start();
         </div>
         <!-- Page Header End -->
 
+
+        <!-- Tabla Start -->
+     
+
         <div class="col-lg-12 col-md-12 ml-auto mr-auto">
                 <div class="table-responsive">
                 <table class="table table-shopping">
@@ -158,88 +162,7 @@ session_start();
     
             </div>
         
-        <!-- Tabla Start -->
-        <table class="table-fill">
-            <thead>
-                <tr>
-                    <th width="5%" class="text-left">ID</th>
-                    <th width="30%" class="text-left">Nombre</th>
-                    <th width="20%" class="text-left">Imagen</th>
-                    <th width="30%" class="text-left">Descripcion</th>
-                    <th width="20%" class="text-left">Departamento</th>
-                    <th width="20%" class="text-left">Talla</th>
-                    <th width="20%" class="text-left">Color</th>
-                    <th width="20%" class="text-left">Estilo</th>
-                    <th width="20%" class="text-left">Existencias</th>
-                    <th width="20%" class="text-left">Precio</th>
-                    <th width="20%" class="text-left"></th>
-                    <th width="20%" class="text-left"></th>
-                    <th width="20%" class="text-left"></th>
-                    
-                </tr>
-            </thead>
-            <tbody class="table-hover">
-                <tr>
-                    
-                <?php
-                    require("../fun/connect_db.php");
-                    $sql="SELECT * FROM prendas";
-                    $result=mysqli_query($conexion,$sql);
-
-                    while ($fila=mysqli_fetch_array($result)) {
-                        $idprenda=$fila['idprenda'];
-                        $nombre=$fila['nom_prenda'];
-                        $img_nombre=$fila['img_nombre'];
-                        $imagen=$fila['img_archivo'];
-                        $descripcion=$fila['descripcion'];
-                        $departamento=$fila['iddepartamento'];
-                        $talla=$fila['idtalla'];
-                        $color=$fila['color'];
-                        $estilo=$fila['idestilo'];
-                        $existencias=$fila['existencias'];
-                        $precio=$fila['precio'];
-                                            
-                        ?>
-                                                                                   
-                        <td class="text-left"><?php echo $idprenda ?></td>
-                        <td class="text-left"><?php echo $nombre ?></td>
-                        <td class="text-left"><img src="<?php echo $imagen ?>" class="card-img-top" alt="..." ></td>
-                        <td class="text-left"><?php echo $descripcion ?></td>
-                        <td class="text-left"><?php echo $departamento ?></td>
-                        <td class="text-left"><?php echo $talla ?></td>
-                        <td class="text-left"><?php echo $color ?></td>
-                        <td class="text-left"><?php echo $estilo ?></td>
-                        <td class="text-left"><?php echo $existencias ?> Unidades</td>
-                        <td class="text-left">$<?php echo $precio ?></td>
-
-                        <th width="10%" class="text-center">
-                            <form action="modificargaleria.php" method="post">
-                                <button class="btn btn-danger" type="submit" name="btnAction" value="eliminar">Galeria</button>
-                            </form>
-                        </th>
-                        <th width="5%" class="text-center">
-                            <form action="modificarprenda.php" method="post">
-                                <input type="hidden" name="id" value="<?php echo $idprenda ?>">
-                                <button class="btn btn-danger" type="submit" name="btnAction" value="Modiciar">Modificar</button>
-                            </form>
-                        </th>
-                        <th width="5%" class="text-center">
-                            <form action="../fun/eliminarprenda.php" method="post">
-                                <input type="hidden" name="id" value="<?php echo $idprenda ?>">
-                                <input type="hidden" name="nombre" value="<?php echo $img_nombre ?>">
-                                <button class="btn btn-danger" type="submit" name="btnAction" value="Eliminar">Eliminar</button>
-                            </form>
-                            
-                        </th>
-                   
-                </tr>
-               
-                <?php } ?>
-                
-            </tbody>
-        </table>
-
-        
+     
         
         <!-- Tabla end -->
 
